4b81e2643ed098ca39ae688e30f427b79c3d1c03
RedGuy12 <paul@reid-family.org>
2022-11-18 21:14:37 -0600

    a
    Signed-off-by: RedGuy12 <paul@reid-family.org>

---
+++ script.mjs
@@ -0,0 +1,44 @@
+import fileSystem from "node:fs/promises";
+
+const pkg = JSON.parse(await fileSystem.readFile("./package.json", "utf8"));
+
+if (!pkg.devDependencies) {
+	console.log("No dev dependencies found");
+	process.exit();
+}
+
+pkg.devDependencies = Object.fromEntries(Object.entries(pkg.devDependencies).map(([dependency, version]) => {
+	if (!dependency.startsWith("@types/")) return [dependency, version];
+
+	let root = dependency.split("/")[1];
+	if (root.includes("__")) root = `@${root.replace("__", "/")}`;
+
+	const rootVersion = pkg.dependencies[root] || pkg.engines[root];
+	if (rootVersion === undefined) {
+		console.log(`Could not find parent dependency for ${dependency}; ignoring`);
+		return [dependency, version];
+	}
+
+	const transformed = transformVersion(rootVersion) ?? version;
+	if (version !== transformed) console.log(`Bumped ${dependency} from ${version} to ${transformed}`);
+	else console.log(`No change needed to ${dependency}'s version`);
+	return [dependency, transformed];
+}));
+
+function transformVersion(version) {
+	if (version.includes("||"))
+		return version.split("||").map(transformVersion).join("||");
+
+	if (version.includes(" - ")) {
+		const newest = version.split(" - ")[1];
+		return transformVersion(newest);
+	}
+
+	version = version.trim();
+	version = version.replace(/^(?:>=|[>~^])/, "");
+	if (/^[v\d]/.test(version)) return `<=${version}`;
+	if (version.startsWith("<") || ["*", "x", "latest", ""].includes(version)) return version;
+}
+
+await fileSystem.writeFile("./package.json", JSON.stringify(pkg, undefined, 2), "utf8");
+console.log("Completed successfully");
