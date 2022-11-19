8421d8c6131d6ed6a65fb3bd74aa6d896861e0c7
RedGuy12 <paul@reid-family.org>
2022-11-18 21:15:17 -0600

    a
    Signed-off-by: RedGuy12 <paul@reid-family.org>

--- script.mjs
+++ script.mjs
@@ -7,27 +7,29 @@ if (!pkg.devDependencies) {
 	process.exit();
 }
 
-pkg.devDependencies = Object.fromEntries(Object.entries(pkg.devDependencies).map(([dependency, version]) => {
-	if (!dependency.startsWith("@types/")) return [dependency, version];
+pkg.devDependencies = Object.fromEntries(
+	Object.entries(pkg.devDependencies).map(([dependency, version]) => {
+		if (!dependency.startsWith("@types/")) return [dependency, version];
 
-	let root = dependency.split("/")[1];
-	if (root.includes("__")) root = `@${root.replace("__", "/")}`;
+		let root = dependency.split("/")[1];
+		if (root.includes("__")) root = `@${root.replace("__", "/")}`;
 
-	const rootVersion = pkg.dependencies[root] || pkg.engines[root];
-	if (rootVersion === undefined) {
-		console.log(`Could not find parent dependency for ${dependency}; ignoring`);
-		return [dependency, version];
-	}
+		const rootVersion = pkg.dependencies[root] || pkg.engines[root];
+		if (rootVersion === undefined) {
+			console.log(`Could not find parent dependency for ${dependency}; ignoring`);
+			return [dependency, version];
+		}
 
-	const transformed = transformVersion(rootVersion) ?? version;
-	if (version !== transformed) console.log(`Bumped ${dependency} from ${version} to ${transformed}`);
-	else console.log(`No change needed to ${dependency}'s version`);
-	return [dependency, transformed];
-}));
+		const transformed = transformVersion(rootVersion) ?? version;
+		if (version !== transformed)
+			console.log(`Bumped ${dependency} from ${version} to ${transformed}`);
+		else console.log(`No change needed to ${dependency}'s version`);
+		return [dependency, transformed];
+	}),
+);
 
 function transformVersion(version) {
-	if (version.includes("||"))
-		return version.split("||").map(transformVersion).join("||");
+	if (version.includes("||")) return version.split("||").map(transformVersion).join("||");
 
 	if (version.includes(" - ")) {
 		const newest = version.split(" - ")[1];
