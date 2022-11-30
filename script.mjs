29672d422ec65cead85d9b13d8e9e53976602472
RedGuy12 <61329810+RedGuy12@users.noreply.github.com>
2022-11-30 07:48:34 -0600

    Update script.mjs

--- script.mjs
+++ script.mjs
@@ -8,26 +8,39 @@ if (!pkg.devDependencies) {
 	process.exit();
 }
 
-pkg.devDependencies = Object.fromEntries(
-	Object.entries(pkg.devDependencies).map(([dependency, version]) => {
-		if (!dependency.startsWith("@types/")) return [dependency, version];
+function main(deps) {
+	return Object.fromEntries(
+		Object.entries(deps).map(([dependency, version]) => {
+			if (!dependency.startsWith("@types/")) {
+				console.log(`[${dependency}] Ignored - Not a @types dependency`);
+				return [dependency, version];
+			}
 
-		let root = dependency.split("/")[1];
-		if (root.includes("__")) root = `@${root.replace("__", "/")}`;
+			let root = dependency.split("/")[1];
+			if (root.includes("__")) root = `@${root.replace("__", "/")}`;
 
-		const rootVersion = pkg.dependencies[root] || pkg.engines[root];
-		if (rootVersion === undefined) {
-			console.log(`Could not find parent dependency for ${dependency}; ignoring`);
-			return [dependency, version];
-		}
+			const rootVersion = pkg.dependencies[root] || pkg.devDependencies[root] || pkg.engines[root];
+			if (rootVersion === undefined) {
+				console.log(`[${dependency}] INGORED - Could not find parent dependency`);
+				return [dependency, version];
+			}
 
-		const transformed = transformVersion(rootVersion) ?? version;
-		if (version !== transformed)
-			console.log(`Bumped ${dependency} from ${version} to ${transformed}`);
-		else console.log(`No change needed to ${dependency}'s version`);
-		return [dependency, transformed];
-	}),
-);
+			const transformed = transformVersion(rootVersion) ?? version;
+			console.log(`[${dependency}] ${
+				version === transformed ?
+				"Ignored - Already synced" :
+				`BUMPED - from ${version} to ${transformed}`
+			}`);
+			return [dependency, transformed];
+		}),
+	);
+}
+
+console.log("Checking devDependencies...");
+pkg.devDependencies = main(deps);
+
+console.log("\nChecking dependencies...");
+pkg.dependencies = main(deps);
 
 function transformVersion(version) {
 	if (version.includes("||")) return version.split("||").map(transformVersion).join("||");
