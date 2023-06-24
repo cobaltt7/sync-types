5fb06ce03431e0fa9376bd12a389ea6136b02ed2
RedGuy12 <61329810+RedGuy12@users.noreply.github.com>
2023-06-24 18:22:27 -0500

    more comment stuff
    Signed-off-by: RedGuy12 <61329810+RedGuy12@users.noreply.github.com>

--- script.mjs
+++ script.mjs
@@ -1,49 +1,50 @@
 import fileSystem from "node:fs/promises";
-
-const unparsed = await fileSystem.readFile("./package.json", "utf8");
-const pkg = JSON.parse(unparsed);
+import pkg from "./package.json" assert { type: "json" };
 
 if (!pkg.devDependencies) {
 	console.log("No dev dependencies found");
 	process.exit();
 }
 
+let hasLogged = false;
+
 function main(deps) {
 	return Object.fromEntries(
 		Object.entries(deps).map(([dependency, version]) => {
-			if (!dependency.startsWith("@types/")) {
-				console.log(`[${dependency}] Ignored - Not a @types dependency`);
-				return [dependency, version];
-			}
+			if (!dependency.startsWith("@types/")) return [dependency, version];
 
 			let root = dependency.split("/")[1];
 			if (root.includes("__")) root = `@${root.replace("__", "/")}`;
 
-			const rootVersion = pkg.dependencies[root] ?? pkg.devDependencies[root] ?? pkg.engines[root];
+			const rootVersion =
+				pkg.dependencies[root] ??
+				pkg.devDependencies[root] ??
+				pkg.engines[root];
 			if (rootVersion === undefined) {
-				console.log(`[${dependency}] INGORED - Could not find parent dependency`);
+				console.log(
+					`**${dependency}**: ignored due to no parent dependency`,
+				);
 				return [dependency, version];
 			}
 
 			const transformed = transformVersion(rootVersion) ?? version;
-			console.log(`[${dependency}] ${
-				version === transformed ?
-				"Ignored - Already synced" :
-				`BUMPED - from ${version} to ${transformed}`
-			}`);
+			if (version !== transformed) {
+				console.log(
+					`**${dependency}**: requirement changed from \`${version}\` to \`${transformed}\``,
+				);
+				hasLogged = true;
+			}
 			return [dependency, transformed];
 		}),
 	);
 }
 
-console.log("Checking devDependencies...");
 pkg.devDependencies = main(pkg.devDependencies);
-
-console.log("\nChecking dependencies...");
 pkg.dependencies = main(pkg.dependencies);
 
 function transformVersion(version) {
-	if (version.includes("||")) return version.split("||").map(transformVersion).join("||");
+	if (version.includes("||"))
+		return version.split("||").map(transformVersion).join("||");
 
 	if (version.includes(" - ")) {
 		const newest = version.split(" - ")[1];
@@ -53,8 +54,14 @@ function transformVersion(version) {
 	version = version.trim();
 	version = version.replace(/^(?:>=|[>~^])/, "");
 	if (/^[v\d]/.test(version)) return `<=${version}`;
-	if (version.startsWith("<") || ["*", "x", "latest", ""].includes(version)) return version;
+	if (version.startsWith("<") || ["*", "x", "latest", ""].includes(version))
+		return version;
 }
 
-await fileSystem.writeFile("./package.json", JSON.stringify(pkg, undefined, unparsed.match(/[ \t]+/)[0] ?? 2) + "\n", "utf8");
-console.log("Completed successfully");
+await fileSystem.writeFile(
+	"./package.json",
+	JSON.stringify(pkg, undefined, unparsed.match(/[ \t]+/)[0] ?? 2) + "\n",
+	"utf8",
+);
+
+if (!hasLogged) console.log("*No requirements changed.*");
